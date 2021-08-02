1fd03e6f1b80c1829d8285f175702cb29c652115
ABHIJIT MONDAL <70851067+abhijitmondal121@users.noreply.github.com>
2021-08-02 23:45:53 +0530

    Update register.php

--- register.php
+++ register.php
@@ -85,7 +85,7 @@ include "partials/dbconnect.php";
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Blood Bank!</title>
+    <title>Blood Bank</title>
   </head>
   <style>
 
